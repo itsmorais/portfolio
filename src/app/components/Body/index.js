6416677fee20c7c148e115a1798a43e8919cf3ce
michaelnexus <michael_morais@outlook.com.br>
2023-03-09 20:57:40 -0300

    Iniciando responsividade

--- src/app/components/Body/index.js
+++ src/app/components/Body/index.js
@@ -15,22 +15,26 @@ export default function Body(props) {
       <div className='body'>
         <div className='projects'>
           {bodyData.map((projeto) => (
-            <div key={projeto.description} className='project'>
+            <div
+              key={projeto.description}
+              className='project'>
               <p>{projeto.description}</p>
               {projeto.link &&
-                <Link href={projeto.link} target={'_blank'}>
-                  <span>Clique aqui para ver o projeto</span>
+                <Link href={projeto.link}
+                  target={'_blank'}>
+                  <span id='linkProjeto'>Clique aqui para ver o projeto</span>
                 </Link>}
-
               <div className={'image-container'}>
                 <Image
                   src={projeto.img}
                   alt={'Imagem de projeto web com javascript'}
                   fill
-                  className='{image}'
-                  sizes="(max-width: 280px) 100vw,(max-width:280px) 50vw,33vw"
+                  className='image'
+
                 >
-                </Image></div>
+                </Image>
+              </div>
+
             </div>
           ))}
         </div>
--- src/app/page.js
+++ src/app/page.js
@@ -37,7 +37,7 @@ export default function Home() {
   ];
 
   let hugeText =
-    'Sou natural de São José dos Campos-SP, fã das ciências humanas e tecnologia.Cursei um ano de licenciatura em história na UNIVAP, continuando minha paixão por meio de livros e palestras.Morei por nove meses em Dublin, na Irlanda, a melhor experiência que já tive.Trabalhei por quatro anos no setor aeronáuticoNamoro a mulher mais incrível que já conheci. Planejamos nos formar juntos, viajar bastante e formar uma família.';
+    'Estou me graduando em Análise e Desenvolvimento de Sistemas pela Faculdade Tecnológica de São Paulo e sou estagiário de Desenvolvimento Full-Stack na Nexus-GeoEngenharia.Além disso, trabalho como freelancer para empresas locais de São José dos Campos-SP, com foco em design visual e desempenho de site, garantindo que os requisitos não funcionais sejam atendidos para a satisfação do usuário.';
 
   if (isProfessional) {
     bodyData = [
@@ -48,12 +48,13 @@ export default function Home() {
       },
       {
         description:
-          'Trabalho em equipe: Por onde passei fiz amigos e sempre mantive um bom relacionamento com todos',
+          'Trabalho em equipe: Por onde passei fiz amigos e sempre mantive um bom relacionamento',
         img: '/port22.png',
       },
     ];
     hugeText =
-      'Estou me graduando em Análise e Desenvolvimento de Sistemas pela Faculdade Tecnológica de São Paulo e sou estagiário de Desenvolvimento Full-Stack na Nexus-GeoEngenharia.Além disso, trabalho como freelancer para empresas locais de São José dos Campos-SP, com foco em design visual e desempenho de site, garantindo que os requisitos não funcionais sejam atendidos para a satisfação do usuário.';
+
+      'Sou natural de São José dos Campos-SP, fã das ciências humanas e tecnologia.Cursei um ano de licenciatura em história na UNIVAP, continuando minha paixão por meio de livros e palestras.Morei por nove meses em Dublin, na Irlanda, a melhor experiência que já tive.Trabalhei por quatro anos no setor aeronáuticoNamoro a mulher mais incrível que já conheci. Planejamos nos formar juntos, viajar bastante e formar uma família.';
   }
 
   return (
